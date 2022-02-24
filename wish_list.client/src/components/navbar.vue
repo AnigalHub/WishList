739e4f4573ec320572d7d4509e5980e12eb21613
AnigalHub <anigal1997@gmail.com>
2022-02-24 22:01:59 +0300

    добавлено меню

--- wish_list.client/src/components/navbar.vue
+++ wish_list.client/src/components/navbar.vue
@@ -1,12 +1,11 @@
 <template>
     <div class="nav">
         <b-container>
-            <div class="background">
-                <div id="TestList">TestList</div>
-                <div>
-
-                </div>
-            </div>
+            <ul class="menu-main">
+                <li><a href="" class="current">Create a wishlist</a></li>
+                <li><a href="">Your wishlists</a></li>
+                <li><a href="">My account</a></li>
+            </ul>
         </b-container>
     </div>
 </template>
@@ -18,11 +17,62 @@
 </script>
 
 <style scoped lang="scss">
+    .menu-main {
+        list-style: none;
+        padding: 20px 0 5px;
+        text-align: center;
+        background: white;
+    }
+    .menu-main li {display: inline-block;}
+    .menu-main li:after {
+        content: "|";
+        color: #606060;
+        display: inline-block;
+        vertical-align:top;
+    }
+    .menu-main li:last-child:after {content: none;}
+    .menu-main a {
+        text-decoration: none;
+        font-family: 'Ubuntu Condensed', sans-serif;
+        letter-spacing: 2px;
+        position: relative;
+        padding-bottom: 20px;
+        margin: 0 34px 0 30px;
+        font-size: 17px;
+        text-transform: uppercase;
+        display: inline-block;
+        transition: color .2s;
+    }
+    .menu-main a, .menu-main a:visited {color: #9d999d;}
+    .menu-main a.current, .menu-main a:hover{color: #feb386;}
+    .menu-main a:before,
+    .menu-main a:after {
+        content: "";
+        position: absolute;
+        height: 4px;
+        top: auto;
+        right: 50%;
+        bottom: -5px;
+        left: 50%;
+        background: #feb386;
+        transition: .8s;
+    }
+    .menu-main a:hover:before, .menu-main .current:before {left: 0;}
+    .menu-main a:hover:after, .menu-main .current:after {right: 0;}
+    @media (max-width: 550px) {
+        .menu-main {padding-top: 0;}
+        .menu-main li {display: block;}
+        .menu-main li:after {content: none;}
+        .menu-main a {
+            padding: 25px 0 20px;
+            margin: 0 30px;
+        }
+    }
     .nav {
         width: 100%;
         height: 66px;
         margin: 0 auto;
-        box-shadow: 0 4px 16px rgba(0, 0, 0, 0.05);
+        box-shadow: 0 1px 16px rgba(0, 0, 0, 0.05);
         border-radius: 0 0 8px 8px;
     }
     #TestList{
