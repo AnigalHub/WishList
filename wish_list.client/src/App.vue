577d388dfe18a1c05a157a102e111f8f5edd6d5f
AnigalHub <anigal1997@gmail.com>
2022-02-24 21:37:25 +0300

    добавлена клиентская часть + установлен бутстрап

---
+++ wish_list.client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <navbar/>
+    <b-container>
+
+      <h1>My WishList</h1>
+    </b-container>
+
+  </div>
+</template>
+
+<script>
+
+
+import Navbar from "@/components/navbar";
+export default {
+  name: 'App',
+  components: {Navbar}
+}
+</script>
+
+<style lang="scss">
+  #app{
+    height: 100%;
+    min-height: 100vh;
+  }
+  body{
+    margin: 0 !important;
+    padding: 0 !important;
+  }
+  .flex-container {
+    display: flex;
+    flex-wrap: wrap;
+  }
+</style>
---
+++ wish_list.client/src/components/navbar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="nav">
+        <b-container>
+            <div class="background">
+                <div id="TestList">TestList</div>
+                <div>
+
+                </div>
+            </div>
+        </b-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "navbar"
+    }
+</script>
+
+<style scoped lang="scss">
+    .nav {
+        width: 100%;
+        height: 66px;
+        margin: 0 auto;
+        box-shadow: 0 4px 16px rgba(0, 0, 0, 0.05);
+        border-radius: 0 0 8px 8px;
+    }
+    #TestList{
+        width: 72px;
+        height: 28px;
+        font-size: 22px;
+        line-height: 28px;
+        color: #59606D;
+        float: left;
+        padding: 18px 0 0 88px;
+    }
+</style>
