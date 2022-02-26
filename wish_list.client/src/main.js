eab8524a4bd2849d1412d25a29b1e1ddfbc8133e
AnigalHub <anigal1997@gmail.com>
2022-02-26 20:37:55 +0300

    добавлен Vue Router

--- wish_list.client/src/main.js
+++ wish_list.client/src/main.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import {BootstrapVue, BootstrapVueIcons} from 'bootstrap-vue';
-
+import VueRouter from 'vue-router'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
+import routes from './routes';
+
+const router = new VueRouter(
+    {
+        mode:'history',
+        routes
+    });
+
 new Vue({
-  render: h => h(App),
+    router,
+    render: function (h) { return h(App) },
 }).$mount('#app')
