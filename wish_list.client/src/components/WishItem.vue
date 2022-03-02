094c16508b901161c7f3c0bb0a54a54ce20f9fc4
AnigalHub <anigal1997@gmail.com>
2022-03-02 19:40:05 +0300

    добавлены комментарии на верстку

--- wish_list.client/src/components/WishItem.vue
+++ wish_list.client/src/components/WishItem.vue
@@ -19,16 +19,20 @@
 </script>
 
 <style scoped lang="scss">
+    /*пункт из списка желаний*/
     #wishItem{
         display: flex;
         box-shadow: 4px 4px 5px -5px rgba(34, 60, 80, 0.8);
         flex-wrap: wrap;
+            /*элементы (картинка, текст, кнопки)*/
             & > div {
                 width: 33%;
                 margin-bottom: 2%;
+                /*последний элемент*/
                 &:last-child{padding-left: 10%;}
             }
     }
+    /*область, куда можно ввести текст с множеством строк*/
     textarea{
         border: 1px solid #bdc0c2 !important;
         resize: none;
@@ -38,16 +42,19 @@
         display: block;
         margin: 20% 0 auto -10%;
     }
+    /*кнопки*/
     button{
         font-size: 1.5rem !important;
         width: 40%;
         margin: 40% 2% 0;
     }
+    /*подпись к элементу*/
     label{
         padding-left: 45%;
         padding-top: 33%;
         font-weight: 400 !important;
         font-size: 3rem;
     }
+    /*подпись к элементу, кнопки*/
     label,button{font-family: 'Ubuntu Condensed', sans-serif;}
 </style>
