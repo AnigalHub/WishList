3b0829e87d8345aabb450b9d8a6d54f78b79ca5d
AnigalHub <anigal1997@gmail.com>
2022-03-03 22:33:59 +0300

    добавлены общие стили двух форм + router-view

--- wish_list.client/src/components/registration.vue
+++ wish_list.client/src/components/registration.vue
@@ -2,11 +2,7 @@
     <div id="registration">
         <navbar-log-in-and-sing-up/>
         <div id="worksheet">
-            <label class="text">Nickname:</label>
-            <input>
-            <label class="text">Password:</label>
-            <input>
-            <b-button class="save" variant="outline-success">Log In</b-button>
+            <router-view/>
         </div>
     </div>
 </template>
@@ -19,7 +15,7 @@
     }
 </script>
 
-<style scoped lang="scss">
+<style  lang="scss">
     #registration{font-size: 1.5rem !important;}
     #worksheet{
         border: 1px solid #bdc0c2 !important;
@@ -28,35 +24,24 @@
         max-width: 750px;
         margin: 0 auto;
     }
-    h2{
-        text-align: center;
-    }
-    #worksheet,button{box-shadow: 5px 5px 5px -5px rgba(34, 60, 80, 0.8);}
+    #worksheet{box-shadow: 5px 5px 5px -5px rgba(34, 60, 80, 0.8);}
+    button{box-shadow: 5px 5px 5px -5px rgba(34, 60, 80, 0.8);}
     input{
         width: 100%;
         margin-bottom: 3%;
     }
     .row{padding-bottom: 3%;}
-    .add,.save,.exit{
-        font-size: 1.5rem !important;
-        font-family: 'Ubuntu Condensed', sans-serif;
+    h2{
+        font-family: 'Roboto Serif', sans-serif;
+        text-align: center;
     }
-    .registration,h2{font-family: 'Roboto Serif', sans-serif;}
     .save{
         display: block;
         width: 50%;
         margin: 0 auto;
-    }
-    .exit,.add{
-        width: 100%;
-    }
-    .exit{
-        margin-top: 6%;
+        font-size: 1.5rem !important;
+        font-family: 'Ubuntu Condensed', sans-serif;
     }
     input,button{height: 45px;}
-    input,.circle{border: 1px solid #bdc0c2 !important;}
-    .circle{
-        background: white !important;
-        margin: 0 auto 6% !important;
-    }
+    input{border: 1px solid #bdc0c2 !important;}
 </style>
