9c53d78988ab5754efa0e939e46226d76ef392ed
AnigalHub <anigal1997@gmail.com>
2022-03-03 21:06:51 +0300

    стили вынесены в общий файл

--- wish_list.client/src/components/logIn.vue
+++ wish_list.client/src/components/logIn.vue
@@ -16,23 +16,5 @@
 </script>
 
 <style scoped lang="scss">
-    button{box-shadow: 5px 5px 5px -5px rgba(34, 60, 80, 0.8);}
-    input{
-        width: 100%;
-        margin-bottom: 3%;
-    }
-    .row{padding-bottom: 3%;}
-    h2{
-        font-family: 'Roboto Serif', sans-serif;
-        text-align: center;
-    }
-    .save{
-        display: block;
-        width: 50%;
-        margin: 0 auto;
-        font-size: 1.5rem !important;
-        font-family: 'Ubuntu Condensed', sans-serif;
-    }
-    input,button{height: 45px;}
-    input{border: 1px solid #bdc0c2 !important;}
+
 </style>
