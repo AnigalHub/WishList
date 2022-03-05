e0fee98808ad4dbe0244fa513a6155f13af4a26d
AnigalHub <anigal1997@gmail.com>
2022-03-05 22:08:04 +0300

    исправлена опечатка файла

---
+++ wish_list.client/src/components/signUp.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="signUp">
+        <h2>Sign Up</h2>
+        <label class="text">Nickname</label>
+        <input>
+        <label class="text">Password</label>
+        <input>
+        <label class="text">Password again</label>
+        <input>
+        <b-button class="send" variant="outline-success">Sign Up</b-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "signUp"
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
