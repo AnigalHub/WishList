134594d02db389400a82f3c2c5744d499426026b
AnigalHub <anigal1997@gmail.com>
2022-03-05 22:11:39 +0300

    стили по таблицам вынесены в App (для всех,любых таблиц)

--- wish_list.client/src/components/wishlists.vue
+++ wish_list.client/src/components/wishlists.vue
@@ -40,28 +40,7 @@
         }
     }
 </script>
-<style lang="scss">
-    #wishlists{
-        thead th:first-child{
-            width: 60%;
-        }
-    }
 
-</style>
 <style scoped lang="scss">
-    /*таблица*/
-    table{
-        margin-top: 2%;
-        font-size: 1.25rem ;
-        font-family: 'Roboto Serif', sans-serif;
-    }
-    /*кнопки в таблице*/
-    table button{
-        font-size: 1.5rem ;
-        font-family: 'Ubuntu Condensed', sans-serif;
-        width: 46%;
-        margin-right: 3%;
-    }
-    /*кнопки, таблица*/
-    button,table{box-shadow: 5px 5px 5px -5px rgba(34, 60, 80, 0.8);}
+
 </style>
