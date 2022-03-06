d5cb615704c0e2cd0ab6a6c3bc3cac72e4ac2ba1
AnigalHub <anigal1997@gmail.com>
2022-03-07 00:33:14 +0300

    добавлен массив на wislists

--- wish_list.client/src/store/Wishlists.js
+++ wish_list.client/src/store/Wishlists.js
@@ -4,15 +4,27 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = () =>({
-    title: "",
-    WishItems:[{}]
+    Wishlists: []
 })
 const getters = {
-
+    Wishlists: (state) => {
+        return state.Wishlists
+    },
 }
 const mutations ={
-
+    addWishlists:(state,name) =>{
+        state.Wishlists.push({name})
+    },
 }
 const actions = {
-
+    addWishlist(context, name){
+        context.commit('addWishlists', {name})
+    },
+}
+export default {
+    namespaced: true,
+    state,
+    actions,
+    getters,
+    mutations,
 }
