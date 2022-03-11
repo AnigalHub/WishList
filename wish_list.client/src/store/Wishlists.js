e3c460ea3414e8c2c00ad76d9656ef5291c97fd5
AnigalHub <anigal1997@gmail.com>
2022-03-11 16:39:34 +0300

    добавлен метод на сохранение измененийЭ

--- wish_list.client/src/store/Wishlists.js
+++ wish_list.client/src/store/Wishlists.js
@@ -4,14 +4,24 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = () =>({
-    Wishlists: []
+    Wishlists: [],
+    Wishlist:[]
 })
 const getters = {
     Wishlists: (state) => {
         return state.Wishlists
     },
+    Wishlist: (state) => {
+        return state.Wishlist
+    },
 }
 const mutations ={
+    copyArray:(state,array) =>{
+       state.Wishlist = array.slice()
+    },
+    addWishItem:(state,wishItem)=>{
+        state.Wishlist.push(wishItem)
+    },
     addWishlist:(state,wishlist) =>{
         state.Wishlists.push(wishlist)
     },
@@ -20,6 +30,12 @@ const mutations ={
     }
 }
 const actions = {
+    copyArray(context, array){
+        context.commit('copyArray', array)
+    },
+    addWishItem(context, wishItem){
+        context.commit('addWishItem', wishItem)
+    },
     addWishlist(context, wishlist){
         context.commit('addWishlist', wishlist)
     },
