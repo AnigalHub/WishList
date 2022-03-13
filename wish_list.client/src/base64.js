bd7eb5d808c7b9d0eadd07d3d12b6a5df88af98a
AnigalHub <anigal1997@gmail.com>
2022-03-13 20:02:41 +0300

    вынесено toBase64 и fromBase64

---
+++ wish_list.client/src/base64.js
@@ -0,0 +1,11 @@
+const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+});
+
+const fromBase64 = async(base64) => {
+    const blob = await (await fetch(base64)).blob()
+    return new File([blob], "test")
+}
