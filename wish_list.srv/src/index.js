d325b020db7bb8359fc7a73b67fd6afd5f4335af
AnigalHub <anigal1997@gmail.com>
2022-03-15 16:43:13 +0300

    добавлено подключение express,cors,srv

---
+++ wish_list.srv/src/index.js
@@ -0,0 +1,16 @@
+const {srv} = require('./cfg.json')
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+const apiRoutes = require("./routes")
+app.use(srv.prefix,apiRoutes)
+
+
+app.listen(srv.port, srv.host,function(){
+    console.log(`Все работает по адресу http://${srv.host}:${srv.port}`);
+});
