4b352ac54e0a1bf5db57344e3f4d3aba2bdc5643
AnigalHub <anigal1997@gmail.com>
2022-03-15 21:02:49 +0300

    добавлены обработчики, express, база и запрос в базу select

---
+++ wish_list.srv/src/routes/index.js
@@ -0,0 +1,18 @@
+// Маршрут
+const api = require("express").Router()
+// Промежуточные обработчики
+const { asyncHandler, errorCatcher } = require("../middlewares")
+// База
+const db = require("./../db")
+
+//Добавить в базу
+api.get('/all', asyncHandler(async (req, res) => {
+    const result = await db.query('SELECT * FROM users')
+    console.log(result)
+    res.end("test")
+}))
+
+
+// Обработчик ошибок
+api.use(errorCatcher)
+module.exports = api
