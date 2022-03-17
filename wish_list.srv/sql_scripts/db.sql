11faed0cdc8894e267245c524a05bfa78cd309e9
AnigalHub <anigal1997@gmail.com>
2022-03-17 21:23:30 +0300

    добавлен пользователь в бд

--- wish_list.srv/sql_scripts/db.sql
+++ wish_list.srv/sql_scripts/db.sql
@@ -25,3 +25,5 @@ SELECT *FROM wish
 GRANT ALL ON users TO lang_user
 GRANT ALL ON wishlist TO lang_user
 GRANT ALL ON wish TO lang_user
+
+INSERT INTO users (loginUser,passwordUser) values ('Anna','757394757')
