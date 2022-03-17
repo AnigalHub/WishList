748511c162c79ec7b75905dca7c68090a7b866a1
AnigalHub <anigal1997@gmail.com>
2022-03-17 21:48:55 +0300

    добавлен запрос к таблице wish

--- wish_list.srv/sql_scripts/db.sql
+++ wish_list.srv/sql_scripts/db.sql
@@ -31,7 +31,12 @@ INSERT INTO users (loginUser,passwordUser) values ('Marya','1234567')
 INSERT INTO wishlist (idUser,title) values ('0b8c0121-d9ba-43c3-ae4c-eb270a3c5e6c','New Year')
 INSERT INTO wishlist (idUser,title) values ('3f6bdfb8-e112-456b-adb9-36cd4770719a','New Year')
 
+SELECT wishlist.id FROM wishlist
+INSERT INTO wish (idWishlist,text) values ('d27a40a1-c2c0-4c11-9da0-0fde59a15174','buy cat')
+
 SELECT users.id, users.loginUser, users.passwordUser,  wishlist.title FROM users, wishlist
 WHERE users.id = wishlist.idUser AND wishlist.idUser = '3f6bdfb8-e112-456b-adb9-36cd4770719a'
 
 DELETE FROM users WHERE users.id = '3f6bdfb8-e112-456b-adb9-36cd4770719a'
+
+
