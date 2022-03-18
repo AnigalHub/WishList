f710322826319bc0580351e8c45eac05872c50af
AnigalHub <anigal1997@gmail.com>
2022-03-18 23:32:33 +0300

    добавлен loadConfig

--- wish_list.client/src/store/ConfigStore.js
+++ wish_list.client/src/store/ConfigStore.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
@@ -14,12 +15,20 @@ const getters = {
 const mutations ={
     setUrl:(state,payload)=>{
         state.url = payload
-    }
+    },
 }
 const actions = {
     setUrl(context,payload){
         context.commit('setUrl',payload)
-    }
+    },
+    loadConfig:async (ctx)=>{
+        const res = (await fetch(`${process.env.BASE_URL}config.json`))
+        const resp = await res.json()
+        console.log("befpre", axios.defaults.baseURL)
+        axios.defaults.baseURL = resp.url
+        console.log("after", axios.defaults.baseURL)
+        ctx.commit("setUrl",resp.url)
+    },
 }
 
 export default {
