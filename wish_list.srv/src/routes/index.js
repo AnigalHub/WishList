d4da04b9fa4d3687a6a73fa4f3329d247cf80afa
AnigalHub <anigal1997@gmail.com>
2022-03-18 23:38:24 +0300

    добавлен запрос insrt into (не работает)

--- wish_list.srv/src/routes/index.js
+++ wish_list.srv/src/routes/index.js
@@ -15,10 +15,18 @@ api.get('/users', asyncHandler(async (req, res) => {
 api.get('/wishlist', asyncHandler(async (req, res) => {
     const result = await db.query('SELECT * FROM wishlist')
     console.log(result)
-    res.end("test")
+    res.end(result.rows)
+
 }))
 api.get('/wish', asyncHandler(async (req, res) => {
     const result = await db.query('SELECT * FROM wish')
+    console.log(result)
+    res.end("test")
+}))
+
+api.get('/AddUsers', asyncHandler(async (req, res) => {
+    const data = req.body
+    const result = await db.query('INSERT INTO users (loginUser,passwordUser) values ($1,$2)',[data.loginUser,data.passwordUser])
     console.log(result)
     res.end("test")
 }))
