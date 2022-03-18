232ee12ba94f5cb71577efdc81d68e025323b5af
AnigalHub <anigal1997@gmail.com>
2022-03-18 23:33:18 +0300

    добавлен router.beforeEach - загрузка config.json

--- wish_list.client/src/main.js
+++ wish_list.client/src/main.js
@@ -17,11 +17,13 @@ import vuex from 'vuex'
 Vue.use(vuex)
 import newWishList from './store/NewWishlist'
 import wishlists from './store/Wishlists'
+import config from './store/ConfigStore'
 
 const store = new vuex.Store({
     modules:{
         newWishlist: newWishList,
         wishlists:wishlists,
+        config:config,
     }
 })
 
@@ -30,13 +32,13 @@ const router = new VueRouter(
         mode:'history',
         routes
     });
-
+router.beforeEach(async(to,from,next)=>{
+    if(!store.getters['config/url'])
+        await store.dispatch('config/loadConfig')
+    await next()
+})
 new Vue({
     router,
     store,
-    async created() {
-        let config = await fetch('config.json')
-
-    },
     render: function (h) { return h(App) }
 }).$mount('#app')
