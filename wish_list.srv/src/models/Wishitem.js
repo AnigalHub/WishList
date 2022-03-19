e2689d92f4e2fd96bd79c6f803d93060e19f612f
AnigalHub <anigal1997@gmail.com>
2022-03-19 22:51:41 +0300

    изменено название idWishlist - idwishlist (как в бд)

--- wish_list.srv/src/models/Wishitem.js
+++ wish_list.srv/src/models/Wishitem.js
@@ -1,14 +1,14 @@
 class WishItem {
     constructor() {
         this.id = ""
-        this.idWishlist = ""
+        this.idwishlist = ""
         this.text = ""
         this.img = null
     }
 
     read(row){
         this.id = row.id
-        this.idWishlist = row.idwishlist
+        this.idwishlist = row.idwishlist
         this.text = row.text;
         this.img = row.img
     }
