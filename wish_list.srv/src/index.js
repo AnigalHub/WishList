3a0ab6841cf9fe1457deb3ab65c918988acfc5c4
AnigalHub <anigal1997@gmail.com>
2022-03-20 22:35:09 +0300

    добавлен limit

--- wish_list.srv/src/index.js
+++ wish_list.srv/src/index.js
@@ -3,7 +3,9 @@ const express = require("express")
 const cors = require("cors")
 
 const app = express()
-app.use(express.json())
+app.use(express.json({
+    limit: '50mb'
+}))
 app.use(cors())
 
 
