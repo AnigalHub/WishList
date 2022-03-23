cce9c428162347b06156cdf17e4d77a61dfa8269
AnigalHub <anigal1997@gmail.com>
2022-03-23 21:12:41 +0300

    половина запроса на удаление wishlists

--- wish_list.srv/src/routes/index.js
+++ wish_list.srv/src/routes/index.js
@@ -15,12 +15,20 @@ api.get('/wishlist', asyncHandler(async (req, res) => {
     res.json(result)
 }))
 
-api.post('/addwishlist', asyncHandler(async (req, res) => {
+api.post('/addWishlist', asyncHandler(async (req, res) => {
+    console.log(req.body)
     const result = await db.query('INSERT INTO wishlist (title) values ($1) RETURNING *;',[req.body.title])
     const wishlistId = result.rows[0].id
     for (const x of req.body.wishItems) {
        await db.query('INSERT INTO wish (text,idwishlist,img) values ($1,$2,$3) RETURNING *;',[x.text,wishlistId,x.img])
     }
+    res.end()
+}))
+
+api.post('/deleteWishlist', asyncHandler(async (req, res) => {
+    console.log(req.body)
+    const result = await db.query('DELETE FROM wishlist WHERE wishlist.id = ($1) RETURNING *;',[req.body.id])
+    console.log(result.rows)
     res.end()
 }))
 
