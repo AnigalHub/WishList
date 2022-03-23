5f749baf05c8b9cca5de919e47c6acc06408fa3f
AnigalHub <anigal1997@gmail.com>
2022-03-23 20:36:04 +0300

    изменен массив с WishItems на wishItems - под один стиль все

--- wish_list.srv/src/models/Wishlist.js
+++ wish_list.srv/src/models/Wishlist.js
@@ -2,13 +2,13 @@ class Wishlist {
     constructor() {
         this.id = ''
         this.title = ""
-        this.WishItems = []
+        this.wishItems = []
     }
 
     read(row){
         this.id = row.id
         this.title = row.title;
-        this.WishItems = []
+        this.wishItems = []
     }
 }
 module.exports = Wishlist
