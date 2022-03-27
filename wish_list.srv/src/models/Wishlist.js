586fdcf0a277f98309842777218ccc2f3f61c881
AnigalHub <anigal1997@gmail.com>
2022-03-27 20:25:09 +0300

    добавлен readFromBody и в нем вызывается readFromBody из WishItem

--- wish_list.srv/src/models/Wishlist.js
+++ wish_list.srv/src/models/Wishlist.js
@@ -1,3 +1,6 @@
+const uuid = require('uuid')
+const WishItem = require("./Wishitem.js")
+
 class Wishlist {
     constructor() {
         this.id = ''
@@ -9,6 +12,23 @@ class Wishlist {
         this.id = row.id
         this.title = row.title;
         this.wishItems = []
+    }
+    readFromBody(body){
+        if(body.id){
+            this.id = body.id
+        }
+        else {
+            this.id = uuid.v4();
+        }
+        this.title = body.title;
+
+        for(let i=0; i<body.wishItems.length; i++ ){
+            const wishItem = new WishItem()
+           wishItem.readFromBody(body.wishItems[i],this.id)
+            this.wishItems.push(wishItem)
+
+        }
+
     }
 }
 module.exports = Wishlist
