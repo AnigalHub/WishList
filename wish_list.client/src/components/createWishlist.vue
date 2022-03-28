6d3fa2855c0a9b4b53f585d319420fe16dc38b80
AnigalHub <anigal1997@gmail.com>
2022-03-28 20:37:50 +0300

    добавлен в computed - everyone - get и set

--- wish_list.client/src/components/createWishlist.vue
+++ wish_list.client/src/components/createWishlist.vue
@@ -9,14 +9,14 @@
         <b-button class="add" @click="addProduct()" variant="outline-secondary">Add Product</b-button>
         <b-row>
             <b-col>
-                <b-button class="save" variant="outline-success" @click="saveProduct(title,wishItems)">Save WishList</b-button>
+                <b-button class="save" variant="outline-success" @click="saveProduct(title,wishItems,everyone)">Save WishList</b-button>
             </b-col>
             <b-col>
                 <b-row>
                     <b-col class="text">
                         Wishlist is visible to all users:
-                        <input class="custom-checkbox" type="checkbox" id="all" v-model="all" >
-                        <label for="all"></label>
+                        <input class="custom-checkbox" type="checkbox" id="everyone" v-model="everyone" >
+                        <label for="everyone"></label>
                     </b-col>
                 </b-row>
             </b-col>
@@ -33,7 +33,6 @@
         components: {WishItem, ImageToUpload},
         data(){
             return{
-                all:false,
                 errorMessage:'',
                 /**Столбцы таблицы */
                 fields:[
@@ -47,14 +46,15 @@
             addProduct(){
                 this.$store.dispatch('newWishlist/addProduct',{text:"", img:null})
             },
-           async saveProduct(title,wishItems){
+           async saveProduct(title,wishItems,everyone){
                if(this.title == ''){
                    this.errorMessage = 'Table name is not specified. You can\'t find it on your wishlist'
                    return
                }
-               await this.$store.dispatch('wishlists/addWishlist',{title,wishItems})
+               await this.$store.dispatch('wishlists/addWishlist',{title,wishItems,everyone})
                await this.$store.dispatch('newWishlist/cleanWishItems')
                this.title = ''
+               this.everyone = false
             },
             deleteProduct(index){
                 this.$store.dispatch('newWishlist/deleteProduct',index)
@@ -70,6 +70,14 @@
                 },
                 set(value){
                     return this.$store.dispatch('newWishlist/setTitle',value)
+                }
+            },
+            everyone:{
+                get(){
+                    return this.$store.getters['newWishlist/everyone']
+                },
+                set(value){
+                    return this.$store.dispatch('newWishlist/setEveryone',value)
                 }
             }
         }
