75d64bfefa3f61c74746e8a59d86f88bd1764270
AnigalHub <anigal1997@gmail.com>
2022-03-29 21:26:19 +0300

    добавлен доступ через module.exports

--- wish_list.srv/src/repositories/UsersRepo.js
+++ wish_list.srv/src/repositories/UsersRepo.js
@@ -3,6 +3,8 @@ class UsersRepo {
         this.db = db
     }
     async AddUser(reqBody){
+        console.log(reqBody)
         await this.db.query ('INSERT INTO users (id, loginUser,passwordUser) values ($1,$2,$3)',[reqBody.id,reqBody.loginUser, reqBody.passwordUser])
     }
 }
+module.exports = UsersRepo
