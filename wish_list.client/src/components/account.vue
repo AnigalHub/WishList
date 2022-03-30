a218b57017ef4d4f22a2028a925ae0014685b286
AnigalHub <anigal1997@gmail.com>
2022-03-30 22:50:13 +0300

    в кнопки добавлено условие

--- wish_list.client/src/components/account.vue
+++ wish_list.client/src/components/account.vue
@@ -6,12 +6,12 @@
            <b-row>
                 <b-col class="text" cols="3">Nickname:</b-col>
                 <b-col><input v-model="name"></b-col>
-               <b-col cols="2"><b-button class="save" variant="outline-success">✓</b-button></b-col>
+               <b-col cols="2"><b-button class="save" variant="outline-success" v-if="name != ''">✓</b-button></b-col>
            </b-row>
            <b-row>
                <b-col class="text" cols="3">Password:</b-col>
                <b-col><input v-model="password"></b-col>
-               <b-col cols="2"><b-button class="save" variant="outline-success">✓</b-button></b-col>
+               <b-col cols="2"><b-button class="save" variant="outline-success" v-if="password != ''">✓</b-button></b-col>
            </b-row>
            <b-row>
                 <b-col class="text" cols="6">Wishlists from other users: {{wishlists}}</b-col>
