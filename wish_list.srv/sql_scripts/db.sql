c8d55c86e81c0abdd7f685ea07c22feb3258d751
AnigalHub <anigal1997@gmail.com>
2022-03-30 22:41:11 +0300

    удалены лишние запросы

--- wish_list.srv/sql_scripts/db.sql
+++ wish_list.srv/sql_scripts/db.sql
@@ -1,21 +1,15 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
+--создание таблиц
 CREATE TABLE users(
 	id uuid PRIMARY KEY DEFAULT (uuid_generate_v4()),
 	loginUser text,
 	passwordUser text
 )
-SELECT *FROM users
-INSERT INTO users (loginUser,passwordUser) values ('Varya','987654') 
-
 CREATE TABLE wishlist(
 	id uuid PRIMARY KEY DEFAULT (uuid_generate_v4()),
 	idUser uuid,
 	title text not null
 )
-SELECT * FROM wishlist
-SELECT * FROM wish
-
 CREATE TABLE wish(
 	id uuid PRIMARY KEY DEFAULT (uuid_generate_v4()),
 	idWishlist uuid not null,
@@ -23,26 +17,29 @@ CREATE TABLE wish(
 	img text
 )
 
---ALTER TABLE wish
---	ALTER COLUMN img TYPE text;
+SELECT *FROM users
 
-ALTER TABLE wishlist
-ADD me boolean
+
+SELECT * FROM wishlist
+
+
+
 SELECT *FROM wish
 
+--права
 GRANT ALL ON users TO lang_user
 GRANT ALL ON wishlist TO lang_user
 GRANT ALL ON wish TO lang_user
 
+--добавление в таблицы
+INSERT INTO users (loginUser,passwordUser) values ('Varya','987654') 
 INSERT INTO users (loginUser,passwordUser) values ('Anna','757394757')
-INSERT INTO users (loginUser,passwordUser) values ('Marya','1234567')
 INSERT INTO wishlist (idUser,title) values ('0b8c0121-d9ba-43c3-ae4c-eb270a3c5e6c','New Year')
-INSERT INTO wishlist (idUser,title) values ('3f6bdfb8-e112-456b-adb9-36cd4770719a','New Year')
-
-SELECT wishlist.id FROM wishlist
 INSERT INTO wish (idWishlist,text) values ('d27a40a1-c2c0-4c11-9da0-0fde59a15174','buy cat')
 
-SELECT users.id, users.loginUser, users.passwordUser,  wishlist.title FROM users, wishlist
+
+SELECT wishlist.id FROM wishlist
+SELECT users.id, users.loginUser, users.passwordUser, wishlist.title FROM users, wishlist
 WHERE users.id = wishlist.idUser AND wishlist.idUser = '3f6bdfb8-e112-456b-adb9-36cd4770719a'
 
 select u.id, u.loginUser, u.passwordUser,wl.title
@@ -52,12 +49,9 @@ INNER JOIN wishlist wl ON wl.idUser = u.id
 SELECT * FROM wishlist
 SELECT * FROM wish
 
-ALTER TABLE wishlist DROP COLUMN me
-
+--удаление из таблицы
 DELETE FROM wish WHERE wish.id = '67633d0a-3e15-40b0-9d01-8343077aa39f'
 DELETE FROM wish WHERE wish.text = ''
 
-DELETE FROM wish WHERE wish.id in ('63633d4c-8f13-489d-b248-b0839ee7d744', '02e33c3e-3169-4e16-88c1-bf19d055f704')
 
-SELECT * FROM wish WHERE id in ('63633d4c-8f13-489d-b248-b0839ee7d744', '02e33c3e-3169-4e16-88c1-bf19d055f704')
 
