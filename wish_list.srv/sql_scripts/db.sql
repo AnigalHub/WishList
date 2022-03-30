7680fd457ffbd7ad6db25c0c3d15356ec45c67c0
AnigalHub <anigal1997@gmail.com>
2022-03-30 22:43:35 +0300

    структурированы запросы - по тому что происходит

--- wish_list.srv/sql_scripts/db.sql
+++ wish_list.srv/sql_scripts/db.sql
@@ -17,13 +17,9 @@ CREATE TABLE wish(
 	img text
 )
 
+--вывод таблиц
 SELECT *FROM users
-
-
 SELECT * FROM wishlist
-
-
-
 SELECT *FROM wish
 
 --права
@@ -37,17 +33,13 @@ INSERT INTO users (loginUser,passwordUser) values ('Anna','757394757')
 INSERT INTO wishlist (idUser,title) values ('0b8c0121-d9ba-43c3-ae4c-eb270a3c5e6c','New Year')
 INSERT INTO wish (idWishlist,text) values ('d27a40a1-c2c0-4c11-9da0-0fde59a15174','buy cat')
 
-
-SELECT wishlist.id FROM wishlist
+-- вывод с условием
 SELECT users.id, users.loginUser, users.passwordUser, wishlist.title FROM users, wishlist
 WHERE users.id = wishlist.idUser AND wishlist.idUser = '3f6bdfb8-e112-456b-adb9-36cd4770719a'
 
-select u.id, u.loginUser, u.passwordUser,wl.title
-FROM users u
+--вывод соединением двух таблиц
+select u.id, u.loginUser, u.passwordUser,wl.title FROM users u
 INNER JOIN wishlist wl ON wl.idUser = u.id
-
-SELECT * FROM wishlist
-SELECT * FROM wish
 
 --удаление из таблицы
 DELETE FROM wish WHERE wish.id = '67633d0a-3e15-40b0-9d01-8343077aa39f'
